694fcbf8cbd1c2e0b24512170b787b9d553c9a8a
Rifqi Ramadhan <rifqyomp@gmail.com>
2022-09-12 11:12:44 +0700

    [fix] enable cors

--- src/main/java/com/enigma/simpletodo/auth/controller/AuthController.java
+++ src/main/java/com/enigma/simpletodo/auth/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
 @Tag(name = "Auth")
 public class AuthController {
 
